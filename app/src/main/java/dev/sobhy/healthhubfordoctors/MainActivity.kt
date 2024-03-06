2d3aaa2dd67340b6670b485e91600bd344571b3c
Sobhy <sobhy.abdelhakam25@gmail.com>
2024-03-06 22:14:23 +0200

    Edit project theme

--- app/src/main/java/dev/sobhy/healthhubfordoctors/MainActivity.kt
+++ app/src/main/java/dev/sobhy/healthhubfordoctors/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.sobhy.healthhubfordoctors.ui.theme.HealthHubForDoctorsTheme
 
 class MainActivity : ComponentActivity() {
