aa74209eac11669c76857a96732816b8c568e962
Sobhy <sobhy.abdelhakam25@gmail.com>
2024-04-15 11:15:59 +0200

    add navigation and localization (Arabic)

--- app/src/main/java/dev/sobhy/healthhubfordoctors/authfeature/presentation/forgetpassword/FPasswordScreen.kt
+++ app/src/main/java/dev/sobhy/healthhubfordoctors/authfeature/presentation/forgetpassword/FPasswordScreen.kt
@@ -5,27 +5,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import dev.sobhy.healthhubfordoctors.R
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Destination
 @Composable
 fun ForgetPasswordScreen(modifier: Modifier = Modifier) {
     val viewModel = viewModel<FPasswordViewModel>()
@@ -35,30 +34,23 @@ fun ForgetPasswordScreen(modifier: Modifier = Modifier) {
             TopAppBar(
                 title = {
                     Text(
-                        text = "Forget Password",
+                        text = stringResource(id = R.string.forgot_password),
                         style = MaterialTheme.typography.headlineLarge,
                     )
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { /*TODO*/ },
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = null,
-                        )
-                    }
                 },
             )
         },
     ) {
         Column(
-            modifier = Modifier.padding(it).fillMaxSize(),
+            modifier =
+                Modifier
+                    .padding(it)
+                    .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
             Text(
-                text = "Enter the email registered with us to be able to change your password",
+                text = stringResource(R.string.forget_password_text),
                 style = MaterialTheme.typography.bodyLarge,
                 textAlign = TextAlign.Center,
                 modifier = Modifier.padding(16.dp),
@@ -71,15 +63,18 @@ fun ForgetPasswordScreen(modifier: Modifier = Modifier) {
                         .fillMaxWidth()
                         .padding(16.dp),
                 label = {
-                    Text(text = "Email",)
-                }
+                    Text(text = stringResource(id = R.string.email))
+                },
             )
             Button(
                 onClick = { /*TODO*/ },
-                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
             ) {
                 Text(
-                    text = "Send",
+                    text = stringResource(R.string.send),
                     style = MaterialTheme.typography.titleLarge,
                 )
             }
