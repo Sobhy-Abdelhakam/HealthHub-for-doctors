d56d83709c6167172f8f4f31d362fc0ef3d4fb01
Sobhy <sobhy.abdelhakam25@gmail.com>
2024-04-27 19:04:51 +0300

    update version of some dependencies

--- app/src/main/java/dev/sobhy/healthhubfordoctors/core/datasource/DataStoreKeys.kt
+++ app/src/main/java/dev/sobhy/healthhubfordoctors/core/datasource/DataStoreKeys.kt
@@ -8,7 +8,6 @@ import androidx.datastore.preferences.preferencesDataStore
 
 object DataStoreKeys {
     const val APP_PREFERENCES = "app_preferences"
-    const val USER_TOKEN = "user_token"
     val IS_USER_LOGGED_IN = booleanPreferencesKey("is_user_logged_in")
 }
 
