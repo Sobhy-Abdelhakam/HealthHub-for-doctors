0966389453d320a76533db6a8ed408a39e4a8215
Sobhy <sobhy.abdelhakam25@gmail.com>
2024-06-21 17:46:12 +0300

    create clinic response class

---
+++ app/src/main/java/dev/sobhy/healthhubfordoctors/clinicfeature/data/response/GetClinicResponse.kt
@@ -0,0 +1,12 @@
+package dev.sobhy.healthhubfordoctors.clinicfeature.data.response
+
+data class GetClinicResponse(
+    val id: Int,
+    val name: String,
+    val address: String,
+    val latitude: Double,
+    val longitude: Double,
+    val examination: Double,
+    val followUp: Double,
+    val doctorId: String,
+)
